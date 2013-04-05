7e87c24e9feb11ff1a4bb5bbe63286bbd0058ea5
JSkelly <joshua.skelton@gmail.com>
2013-04-05 00:23:29 -0500

    Docstrings added to transform module.

--- toast/transform.py
+++ toast/transform.py
@@ -2,7 +2,16 @@ from toast.scene_graph import Component
 from toast.math.vector import Vector2D
 
 class Transform(Component):
+    """Assists with positioning of GameObjects and all it's attached children 
+    with transforms. On changing of position, those children will re-position 
+    themselves relative to their parent's position.
+    """
+    
     def __init__(self, *position):
+        """
+        :param position: The initial position for the transform.
+        :type position: tuple.
+        """
         super(Transform, self).__init__()
         
         if position is ():
@@ -11,42 +20,51 @@ class Transform(Component):
             self.__local_position = Vector2D(position)
             
     @property
-    def grandparent(self):
-        """ Returns the parent of the parent. """
+    def parent(self):
+        """Gets the parent of the GameObject. 
+        
+        :returns: GameObject -- The parent of the parent GameObject.
+        """
         return self.game_object.parent if self.game_object else None
         
     @property
     def position(self):
-        """ Returns the world position as a Vector2D. """
-        if self.grandparent and hasattr(self.grandparent, 'transform'):
-            return self.grandparent.transform.position + self.__local_position
+        """Gets the transform position. 
+        
+        :returns: Vector2D -- The transform position in world space.
+        """
+        if self.parent and hasattr(self.parent, 'transform'):
+            return self.parent.transform.position + self.__local_position
         else:
             return self.local_position
     
     @position.setter
     def position(self, other):
-        """ Sets the position in world space.
+        """Sets the position in world space.
         
-        Arguments:
-        other -- A position in world space.
+        :param other: The new position.
+        :type other: tuple.
         
         """
-        if self.grandparent and hasattr(self.grandparent, 'transform'):
-            self.__local_position = self.grandparent.get_component('Transform').position - other
+        if self.parent and hasattr(self.parent, 'transform'):
+            self.__local_position = self.parent.get_component('Transform').position - other
         else:
             self.local_position = other
         
     @property
     def local_position(self):
-        """ Returns the position relative to it's parent's parent as a Vector2D. """
+        """Gets the position relative to the GameObject's parent. 
+        
+        :returns: Vector2D -- The transform's position in local space.
+        """
         return self.__local_position
     
     @local_position.setter
     def local_position(self, other):
-        """ Sets the position in local(relative to it's parent) space.
+        """Sets the position relative to the GameObject's parent. 
         
-        Arguments:
-        other -- A position in local space.
+        :param other: The new position in the GameObject's parent's local space.
+        :type other: tuple.
         
         """
         self.__local_position = Vector2D(other[0], other[1])
